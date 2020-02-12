4a03511aa5ff1cce83cd296510078864d6405550
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-02-12 10:31:45 +0300

    Added task Настройка локальной стратегии

---
+++ 7-module/1-task/controllers/products.js
@@ -0,0 +1,12 @@
+module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
+  ctx.body = {products: []};
+};
+
+module.exports.productList = async function productList(ctx, next) {
+  ctx.body = {products: []};
+};
+
+module.exports.productById = async function productById(ctx, next) {
+  ctx.body = {product: {}};
+};
+
