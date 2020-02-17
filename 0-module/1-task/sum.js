27425a388da979960c95e2a830364e86c0de7023
misha_r82@mail.ru <Mir232du>
2020-02-17 12:34:10 +0300

    added solution for 0 module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,7 @@
 function sum(a, b) {
-  /* ваш код */
+    if (typeof(a) != "number" || typeof(b) != "number")
+        throw new TypeError("Переменные должны быть числами!");
+    return a+b;
 }
 
 module.exports = sum;
